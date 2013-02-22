240fd5db3e689a4c98d24d5d3743a7c51a147b12
Lars Haugan <lars@larshaugan.net>
2013-02-22 12:02:07 +0100

    Implemented and tested the now fully functional handValue(args) and cardPoints(args) functions. Everything should be OK here. Now printing out lots of debug messages. Should probably clean that up sometime

--- class_blackjackGame.cpp
+++ class_blackjackGame.cpp
@@ -17,16 +17,21 @@ namespace casino{
     }
     void blackjackGame::start(){
         // Setting up the deck
-            // Everyday I'm shuffeling
         deck = cards::deck(2);
+
+        // Everyday I'm shuffeling
         deck.shuffle();
 
         std::vector<cards::card> v;
 
-        v.push_back(deck.deal());
-        v.push_back(deck.deal());
-        v.push_back(deck.deal());
-        v.push_back(deck.deal());
+        //v.push_back(deck.deal());
+        //v.push_back(deck.deal());
+        //v.push_back(deck.deal());
+        //v.push_back(deck.deal());
+        v.push_back(cards::card((cards::t_suit)cards::SPADES, (cards::t_rank)'A'));
+        v.push_back(cards::card((cards::t_suit)cards::HEARTS, (cards::t_rank)'A'));
+        v.push_back(cards::card((cards::t_suit)cards::CLUBS, (cards::t_rank)'A'));
+        v.push_back(cards::card((cards::t_suit)cards::DIAMONDS, (cards::t_rank)'A'));
 
         std::cout << "Rank: " << v[0].getRank() << " Points: " << cardPoints(v[0]) << std::endl<<std::endl;
         std::cout << "Rank: " << v[1].getRank() << " Points: " << cardPoints(v[1]) << std::endl<<std::endl; 
@@ -36,6 +41,36 @@ namespace casino{
         std::cout << handValue(v) << std::endl;
         playRound();
         // while playing some rounds m'kay
+    }
+
+    int blackjackGame::handValue(std::vector<cards::card> c){
+        // Gets the *best* value for the hand. Not posible to split any cards here! Sorry
+        int handTotal = 0;
+        int currentValue = 0;
+        std::vector<cards::card> ace;
+
+        // Iterating over the hand of cards
+        for(std::vector<cards::card>::iterator it = c.begin(); it != c.end(); ++it){
+            currentValue = cardPoints(*it); // Getting the value of a single card
+            if(currentValue == 11){
+                ace.push_back(*it);
+            }
+            if(currentValue != 11 && currentValue > 0){
+                handTotal += currentValue;
+            }
+            currentValue = 0;
+        }
+
+        if(ace.size() > 0){
+            for(std::vector<cards::card>::iterator i = ace.begin(); i != ace.end(); ++i){
+                if(handTotal < 11){
+                    handTotal += 11;
+                }else{
+                    handTotal += 1;
+                }
+            }
+        }
+        return handTotal;
     }
 
     // This function is right now not a part of the class
@@ -54,10 +89,5 @@ namespace casino{
             }
         }
         return 0;
-    }
-    int blackjackGame::handValue(std::vector<cards::card> c){
-        int value = 0;
-        value += blackjackGame::cardPoints(c[0]);
-        return value;
     }
 }
