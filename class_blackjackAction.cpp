a34a5c26645b32598fb2fab55da5bf441e809224
Lars Haugan <lars@larshaugan.net>
2013-02-24 22:23:19 +0100

    Impemented funcitons in blackjackAction. Wondering about some initiatlisation issues in action...

--- class_blackjackAction.cpp
+++ class_blackjackAction.cpp
@@ -1,5 +1,16 @@
 #include "class_blackjackAction.h"
+#include <iostream>
 
 namespace casino{
 
+    blackjackAction::blackjackAction(controlType ct) : action(){
+        // Probably should be initialisation of the actionType?? Not controlType?
+    }
+
+    blackjackAction::atype blackjackAction::getAtype(){
+        return actionType;
+    }
+    void blackjackAction::print(){
+        
+    }
 }
