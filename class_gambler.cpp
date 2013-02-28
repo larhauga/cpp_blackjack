fd3c3b5e07af11875beaaa75e9a91ad41a85a330
Lars Haugan <lars@larshaugan.net>
2013-02-28 17:39:04 +0100

    Implemented part of placebet and some other functions to gambler. Also fixed construcotr

--- class_gambler.cpp
+++ class_gambler.cpp
@@ -1,21 +1,43 @@
 #include "class_gambler.h"
+#include "class_blackjackStrategy.h"
+#include "class_blackjackAction.h"
+#include <iostream>
 
 using namespace std;
 using namespace casino;
 
     gambler::gambler(string fname, string lname){
-        // Set gamlerNr eller gamblerId her
+        
         firstName = fname;
         lastName = lname;
+
     }
     gambler::gambler(controlType ct){
-    
+        static int gamblerNr = 0;
+        gamblerNr++;
+        gamblerId = gamblerNr;
+
+        strategy* s = new blackjackStrategy(ct);
+        strategies.push_back(s);
     }
     void gambler::giveMoney(cash amount){
         wallet += amount;
     }
     cash gambler::placeBet(){
-    
+        // Cin amount?
+        //string* s = new string();
+        int i = 0;
+        try{
+        cout << "Place bet: ";
+        cin >> i;
+        }catch(int e){
+            cout << endl << "Could not get that. Which amount will you bet? ";
+            cin >> i;
+        }
+        // Remove money from wallet, and return the amount.
+        // Cannot plage a bet larger than wallet?
+        // This function is not done!!!!!
+
     }
     string gambler::name(){
         return firstName + " " + lastName;
@@ -26,7 +48,9 @@ using namespace casino;
     }
     
     action* gambler::takeAction(gameState* g){
-        
+        //There will only be one strategy at this time. In future check for gametype
+        action* a = strategies[0]->takeAction(g);
+        return a;
     }
     void gambler::joinGame(game& g){
         readyToPlay = 1;
