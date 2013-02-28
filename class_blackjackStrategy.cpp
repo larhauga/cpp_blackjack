ba26f40d7e4fb0da55e15863ce0495cbc0a4caec
Lars Haugan <lars@larshaugan.net>
2013-02-28 22:33:30 +0100

    Adding print method to takeAction in strategy

--- class_blackjackStrategy.cpp
+++ class_blackjackStrategy.cpp
@@ -6,6 +6,7 @@ namespace casino{
        // Nowhere to initialize the controlType. Missing setters to base or local variable 
     }
     action* blackjackStrategy::takeAction(gameState* g){
+        g->print();
         action* a = new blackjackAction(TERMINAL);
         return a;
     }
