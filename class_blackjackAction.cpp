6e67c909fac8e1d1349beb3ffeaa6e9c128f3b7d
Lars Haugan <lars@larshaugan.net>
2013-03-02 23:02:47 +0100

    Fixed bug where game HITs if you just write som random stuff. Not anymore you dont

--- class_blackjackAction.cpp
+++ class_blackjackAction.cpp
@@ -5,20 +5,26 @@ namespace casino{
 
     blackjackAction::blackjackAction(controlType ct) : action(){
         if(ct == TERMINAL){
-            print();
-            std::string input;
-            std::cin >> input;
-            if(input == "HIT"){
-                blackjackAction::actionType = blackjackAction::HIT;
-            }else if(input == "STAND"){
-                blackjackAction::actionType = blackjackAction::STAND;
-            // Ready to implement new features
-            //}else if(input == "DOUBLE"){
-            //    blackjackAction::actionType = blackjackAction::DOUBLE;
-            //}else if(input == "SPLIT"){
-            //    blackjackAction::actionType = blackjackAction::SPLIT;
-            }else{
-                std::cout << "Unknown action" << std::endl;
+            bool valid = false;
+            while(!valid){
+                print();
+                std::string input;
+                std::cin >> input;
+                if(input == "HIT" || input == "hit"){
+                    blackjackAction::actionType = blackjackAction::HIT;
+                    valid = true;
+                }else if(input == "STAND" || input == "stand"){
+                    blackjackAction::actionType = blackjackAction::STAND;
+                    valid = true;
+                // Ready to implement new features
+                //}else if(input == "DOUBLE"){
+                //    blackjackAction::actionType = blackjackAction::DOUBLE;
+                //}else if(input == "SPLIT"){
+                //    blackjackAction::actionType = blackjackAction::SPLIT;
+                }else{
+                    std::cout << "Unknown action" << std::endl;
+                    valid = false;
+                }
             }
         }else{
 
